e99d1af5548103d3e7b5f2abb02e784e1c5ac985
taozhengkai <tzk19910406@hotmail.com>
2015-08-06 20:38:45 +0800

    添加工具类

--- server/db/util.py
+++ server/db/util.py
@@ -1,232 +1,227 @@
-# -*- coding: UTF-8 -*-
-# 增加datetime.datetime的序列化操作
-import json
-import datetime
-import decimal
-import time
-import app.share.mylog as log
+#coding:utf8
+'''
+Created on 2013-5-8
+
+@author: lan (www.9miao.com)
+'''
+
+from gfirefly.dbentrust.dbpool import dbpool
+from gfirefly.dbentrust import dbutils
+from app.share import dbtransaction
+from numbers import Number
+from gtwisted.utils import log
 import traceback
 
 
-class MyJsonEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime.datetime):
-            return obj.strftime('%Y-%m-%d %H:%M:%S')
-        elif isinstance(obj, datetime.timedelta):
-            return str(obj)
-        elif isinstance(obj, decimal.Decimal):
-            return float(obj)
-        else:
-            return json.JSONEncoder.default(self, obj)
-
-
-class SmartResponse():
-    def __init__(self):
-        pass
-
-    @classmethod
-    def jsonwrap(cls, obj):
-        """
-        将JSON对象转成合适的字符串返回(处理datetime,并且以utf8的格式返回)
-        :param msg:
-        :return:
-        """
-        msg = json.dumps(obj, cls=MyJsonEncoder, ensure_ascii=False)
-
-        if isinstance(msg, unicode):
-            msg = msg.encode("utf8")
-        # for log
-        log.msg("response : %s" % msg)
-        return msg
-
-    @classmethod
-    def strwrap(cls, msg):
-        if isinstance(msg, unicode):
-            msg = msg.encode("utf8")
-
-        return msg
-
-
-class MyStrAList():
+class SQLError(Exception): 
     """
-    list与str互转
+    memcached key error
     """
-    def __init__(self):
-        pass
-
-    @classmethod
-    def str_to_list(cls, s_str, sep=' '):
-        """
-        str => list
-        :param s_str: 源字符串
-        :param sep: 分隔符, 默认为空白符
-        :return:
-        """
-        if not s_str or len(s_str) == 0:
-            return []
-        return s_str.split(sep)
-
-    @classmethod
-    def list_to_str(cls, s_list, sep=' '):
-        """
-        list = > str
-        :param s_list: 源列表
-        :param sep: 分隔符, 默认为空白符
-        :return:
-        """
-        if not s_list or len(s_list) == 0:
-            return ''
-        t_str = sep.join(['%s']*len(s_list)) % tuple(s_list)
-        return t_str
-
-time_formate_str = "%Y-%m-%d %H:%M:%S"
-
-def time_now_str():
-    return time.strftime(time_formate_str, time.localtime())
+    
+    def __str__(self):
+        return "memcache key error"
 
 
-def copy_dict_by_keys(keys, source_dict, to_dict):
-    for key in keys:
-        if key in source_dict.keys():
-            to_dict[key] = source_dict[key]
+def forEachPlusInsertProps(tablename,props):
+    assert isinstance(props, dict)
+    pkeysstr = str(tuple([str(key) for key in props.keys()])).replace('\'','`')
+    pvaluesstr = []
+    for val in props.values():
+        temp = "NULL,"
+        if isinstance(val, Number):
+            temp = "%s,"% val
+        elif val is None:
+            temp = "NULL,"
         else:
-            print key , 'not in ', source_dict
-            assert 0
+            temp = "'%s'," % val.replace("'", "\\'")
+        pvaluesstr.append(temp)
 
-def add_dict_by_keys(dict_1, dict_2, keys=None):
-    if not keys:
-        keys = dict_2.keys()
+    pvaluesstr = ''.join(pvaluesstr)[:-1]
+    sqlstr = """INSERT INTO `%s` %s values (%s);"""%(tablename,pkeysstr,pvaluesstr)
+    return sqlstr
 
-    for key in keys:
-        if key in dict_2.keys():
-            if key in dict_1.keys():
-                dict_1[key] += dict_2[key]
-            else:
-                dict_1[key] = dict_2[key]
-
-
-def minus_dict_by_keys(dict_1, dict_2, keys=None):
-    if not keys:
-        keys = dict_2.keys()
-
-    for key in keys:
-        if key in dict_2.keys():
-            if key in dict_1.keys():
-                dict_1[key] -= dict_2[key]
-
-
-def fill_dict_by_path(paths, target_dict, value):
-    if len(paths) > 1:
-        no_last = paths[:-1]
-        temp_dict = target_dict
-        for path in no_last:
-            if path not in temp_dict.keys():
-                temp_dict[path] = {}
-            temp_dict = temp_dict[path]
-        temp_dict[paths[-1]] = value
+def FormatItem(key, value):
+    sqlstr = ""
+    if isinstance(value, Number):
+        sqlstr = " `%s`=%s " % (key, value)
     else:
-        key = paths
-        if isinstance(paths, list):
-            key = paths[0]
-        target_dict[key] = value
+        sqlstr = " `%s`='%s'  " % (key, value.replace("'", "\\'"))
+    return sqlstr
 
+def FormatCondition(props):
+    """生成查询条件字符串
+    """
+    items = props.items()
+    itemstrlist = []
+    for _item in items:
+        if isinstance(_item[1],Number):
+            sqlstr = " `%s`=%s AND"% _item
+        else:
+            sqlstr = " `%s`='%s' AND "%(_item[0], _item[1].replace("'", "\\'"))
+        itemstrlist.append(sqlstr)
+    sqlstr = ''.join(itemstrlist)
+    return sqlstr[:-4]
 
-def get_dict_value_by_path(paths, source_dict):
-    if len(paths) > 1:
-        no_last = paths[:-1]
-        temp_dict = source_dict
-        for path in no_last:
-            if path in temp_dict.keys():
-                temp_dict = temp_dict[path]
-            else:
-                return None
-        return temp_dict.get(paths[-1])
+def FormatUpdateStr(props):
+    """生成更新语句
+    """
+    items = props.items()
+    itemstrlist = []
+    for _item in items:
+        if isinstance(_item[1],Number):
+            sqlstr = " `%s`=%s,"% _item
+        elif _item[1] is None:
+            sqlstr = " `%s`= NULL ,"% (_item[0])
+        else:
+            sqlstr = " `%s`='%s',"% (_item[0], _item[1].replace("'", "\\'"))
+        itemstrlist.append(sqlstr)
+    sqlstr = ''.join(itemstrlist)
+    return sqlstr[:-1]
+    
+def forEachUpdateProps(tablename,props,prere):
+    '''遍历所要修改的属性，以生成sql语句'''
+    assert isinstance(props, dict)
+    pro = FormatUpdateStr(props)
+    pre = FormatCondition(prere)
+    sqlstr = """UPDATE `%s` SET %s WHERE %s;"""% (tablename,pro,pre)
+    return sqlstr
+
+def EachQueryProps(props):
+    '''遍历字段列表生成sql语句
+    '''
+    sqlstr = ""
+    if props == '*':
+        return '*'
+    elif type(props) == type([0]):
+        for prop in props:
+            sqlstr = sqlstr + prop +','
+        sqlstr = sqlstr[:-1]
+        return sqlstr
     else:
-        key = paths
-        if isinstance(paths, list):
-            key = paths[0]
-        return source_dict.get(key)
+        raise Exception('props to query must be dict')
+        return
+
+def forEachQueryProps(sqlstr, props):
+    '''遍历所要查询属性，以生成sql语句'''
+    if props == '*':
+        sqlstr += ' *'
+    elif type(props) == type([0]):
+        i = 0
+        for prop in props:
+            if(i == 0):
+                sqlstr += ' ' + prop
+            else:
+                sqlstr += ', ' + prop
+            i += 1
+    else:
+        raise Exception('props to query must be list')
+        return
+    return sqlstr
+
+def GetTableIncrValue(tablename):
+    """
+    """
+    conn = dbpool.connection(write=False,tablename=tablename)
+    database = conn._conn._kwargs.get('db')
+    sql = """SELECT AUTO_INCREMENT FROM information_schema.`TABLES` \
+    WHERE TABLE_SCHEMA='%s' AND TABLE_NAME='%s';"""%(database,tablename)
+    result = dbtransaction.querySql(conn, sql, is_fetch_one=True)
+    if result:
+        return result[0]
+    return result
+
+def ReadDataFromDB(tablename):
+    """
+    """
+    sql = """select * from %s"""%tablename
+    conn = dbpool.connection(write=False,tablename=tablename)
+    result = dbtransaction.querySql(conn, sql, dictcursor=True)
+    return result
+
+def DeleteFromDB(tablename,props):
+    '''从数据库中删除
+    '''
+    prers = FormatCondition(props)
+    sql = """DELETE FROM %s WHERE %s ;"""%(tablename,prers)
+    conn = dbpool.connection(write=True,tablename=tablename)
+    result = dbtransaction.execSql(conn, sql)
+    return result
+
+def InsertIntoDBAndReturnID(tablename,data):
+    """写入数据库,并返回ID
+    """
+    sql = forEachPlusInsertProps(tablename,data)
+    conn = dbpool.connection(write=True,tablename=tablename)
+    result, row_id = dbtransaction.execSql(conn, sql, get_row_id=True)
+    return row_id
+
+def InsertIntoDB(tablename,data):
+    """写入数据库
+    """
+    sql = forEachPlusInsertProps(tablename,data)
+    conn = dbpool.connection(write=True,tablename=tablename)
+    result = dbtransaction.execSql(conn, sql)
+    return result
+
+def UpdateWithDict(tablename,props,prere):
+    """更新记录
+    """
+    sql = forEachUpdateProps(tablename, props, prere)
+    conn = dbpool.connection(write=True,tablename=tablename)
+    result = dbtransaction.execSql(conn, sql)
+    return result
+
+def getAllPkByFkInDB(tablename,pkname,props):
+    """根据所有的外键获取主键ID
+    """
+    props = FormatCondition(props)
+    sql = """Select %s from `%s` where %s"""%(pkname,tablename,props)
+    conn = dbpool.connection(write=False,tablename=tablename)
+    result = dbtransaction.querySql(conn, sql)
+    return result
+
+def GetOneRecordInfo(tablename,props):
+    '''获取单条数据的信息
+    '''
+    props = FormatCondition(props)
+    sql = """Select * from `%s` where %s"""%(tablename,props)
+    conn = dbpool.connection(write=False,tablename=tablename)
+    result = dbtransaction.querySql(conn, sql, dictcursor=True, is_fetch_one=True)
+    return result
+
+def GetRecordList(tablename,pkname,pklist):
+    """
+    """
+    pkliststr = ""
+    for pkid in pklist:
+        pkliststr+="%s,"%pkid
+    pkliststr = "(%s)"%pkliststr[:-1]
+    sql = """SELECT * FROM `%s` WHERE `%s` IN %s;"""%(tablename,pkname,pkliststr)
+    conn = dbpool.connection(write=False,tablename=tablename)
+    result = dbtransaction.querySql(conn, sql, dictcursor=True)
+    return result
+
+def DBTest():
+    sql = """SELECT * FROM tb_item WHERE characterId=1000001;"""
+    conn = dbpool.connection(write=False,tablename="tb_item")
+    result = dbtransaction.querySql(conn, sql, dictcursor=True)
+    return result
+
+def getallkeys(key,redismem):
+    """
+    根据通配符进行keys的筛选
+    :param key:  通配符
+    :param redismem: redis client对象
+    :return: 符合通配符的所有 主键 id
+    """
+    itemsinfo = redismem.keys(key)
+    itemindex = []
+    for items in itemsinfo:
+        itemindex += [ items.split(':')[1]]
+    return itemindex
+
+def getAllPkByFkInMEM(key,fk,mem):
+    pass
 
 
-def safe_str_to_int(value):
-    try:
-        if isinstance(value, int):
-                return value
-        if value:
-                return int(value)
-        else:
-            return None
-    except Exception, e:
-        print traceback.format_exc()
-        return 0
 
-
-def safe_str_to_list(value):
-    try:
-        if isinstance(value, list):
-            return value
-        if value:
-            return eval(value)
-        else:
-            return None
-    except Exception, e:
-        print traceback.format_exc()
-        return []
-
-
-def safe_list_to_str(value):
-    try:
-        if isinstance(value, str):
-            return value
-        if value:
-            return repr(value)
-        else:
-            return None
-    except Exception, e:
-        print traceback.format_exc()
-        return '[]'
-
-
-def safe_str_to_dict(value):
-    try:
-        if isinstance(value, dict):
-            return value
-        if value:
-            return eval(value)
-        else:
-            return None
-    except Exception, e:
-        print traceback.format_exc()
-        return {}
-
-
-#把datetime转成字符串
-def datetime_toString(dt):
-    return dt.strftime(time_formate_str)
-
-#把字符串转成datetime
-def string_toDatetime(string):
-    return datetime.datetime.strptime(string, time_formate_str)
-
-#把字符串转成时间戳形式
-def string_toTimestamp(strTime):
-    return time.mktime(string_toDatetime(strTime).timetuple())
-
-#把时间戳转成字符串形式
-def timestamp_toString(stamp):
-    return time.strftime(time_formate_str, time.localtime(stamp))
-
-#把datetime类型转成时间戳形式
-def datetime_toTimestamp(dateTim):
-    return time.mktime(dateTim.timetuple())
-
-
-def is_datetime_equal(a_date_time, b_date_time):
-    if a_date_time.year == b_date_time.year and a_date_time.month == b_date_time.month \
-            and a_date_time.day == b_date_time.day:
-        return True
-    return False
-
-
-def time_delta_toString(timeDelta):
-    return '{:02}:{:02}:{:02}'.format(timeDelta.seconds // 3600, timeDelta.seconds % 3600 // 60, timeDelta.seconds % 60)
---
+++ server/tool/util.py
@@ -0,0 +1,232 @@
+# -*- coding: UTF-8 -*-
+# 增加datetime.datetime的序列化操作
+import json
+import datetime
+import decimal
+import time
+import app.share.mylog as log
+import traceback
+
+
+class MyJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(obj, datetime.timedelta):
+            return str(obj)
+        elif isinstance(obj, decimal.Decimal):
+            return float(obj)
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
+class SmartResponse():
+    def __init__(self):
+        pass
+
+    @classmethod
+    def jsonwrap(cls, obj):
+        """
+        将JSON对象转成合适的字符串返回(处理datetime,并且以utf8的格式返回)
+        :param msg:
+        :return:
+        """
+        msg = json.dumps(obj, cls=MyJsonEncoder, ensure_ascii=False)
+
+        if isinstance(msg, unicode):
+            msg = msg.encode("utf8")
+        # for log
+        log.msg("response : %s" % msg)
+        return msg
+
+    @classmethod
+    def strwrap(cls, msg):
+        if isinstance(msg, unicode):
+            msg = msg.encode("utf8")
+
+        return msg
+
+
+class MyStrAList():
+    """
+    list与str互转
+    """
+    def __init__(self):
+        pass
+
+    @classmethod
+    def str_to_list(cls, s_str, sep=' '):
+        """
+        str => list
+        :param s_str: 源字符串
+        :param sep: 分隔符, 默认为空白符
+        :return:
+        """
+        if not s_str or len(s_str) == 0:
+            return []
+        return s_str.split(sep)
+
+    @classmethod
+    def list_to_str(cls, s_list, sep=' '):
+        """
+        list = > str
+        :param s_list: 源列表
+        :param sep: 分隔符, 默认为空白符
+        :return:
+        """
+        if not s_list or len(s_list) == 0:
+            return ''
+        t_str = sep.join(['%s']*len(s_list)) % tuple(s_list)
+        return t_str
+
+time_formate_str = "%Y-%m-%d %H:%M:%S"
+
+def time_now_str():
+    return time.strftime(time_formate_str, time.localtime())
+
+
+def copy_dict_by_keys(keys, source_dict, to_dict):
+    for key in keys:
+        if key in source_dict.keys():
+            to_dict[key] = source_dict[key]
+        else:
+            print key , 'not in ', source_dict
+            assert 0
+
+def add_dict_by_keys(dict_1, dict_2, keys=None):
+    if not keys:
+        keys = dict_2.keys()
+
+    for key in keys:
+        if key in dict_2.keys():
+            if key in dict_1.keys():
+                dict_1[key] += dict_2[key]
+            else:
+                dict_1[key] = dict_2[key]
+
+
+def minus_dict_by_keys(dict_1, dict_2, keys=None):
+    if not keys:
+        keys = dict_2.keys()
+
+    for key in keys:
+        if key in dict_2.keys():
+            if key in dict_1.keys():
+                dict_1[key] -= dict_2[key]
+
+
+def fill_dict_by_path(paths, target_dict, value):
+    if len(paths) > 1:
+        no_last = paths[:-1]
+        temp_dict = target_dict
+        for path in no_last:
+            if path not in temp_dict.keys():
+                temp_dict[path] = {}
+            temp_dict = temp_dict[path]
+        temp_dict[paths[-1]] = value
+    else:
+        key = paths
+        if isinstance(paths, list):
+            key = paths[0]
+        target_dict[key] = value
+
+
+def get_dict_value_by_path(paths, source_dict):
+    if len(paths) > 1:
+        no_last = paths[:-1]
+        temp_dict = source_dict
+        for path in no_last:
+            if path in temp_dict.keys():
+                temp_dict = temp_dict[path]
+            else:
+                return None
+        return temp_dict.get(paths[-1])
+    else:
+        key = paths
+        if isinstance(paths, list):
+            key = paths[0]
+        return source_dict.get(key)
+
+
+def safe_str_to_int(value):
+    try:
+        if isinstance(value, int):
+                return value
+        if value:
+                return int(value)
+        else:
+            return None
+    except Exception, e:
+        print traceback.format_exc()
+        return 0
+
+
+def safe_str_to_list(value):
+    try:
+        if isinstance(value, list):
+            return value
+        if value:
+            return eval(value)
+        else:
+            return None
+    except Exception, e:
+        print traceback.format_exc()
+        return []
+
+
+def safe_list_to_str(value):
+    try:
+        if isinstance(value, str):
+            return value
+        if value:
+            return repr(value)
+        else:
+            return None
+    except Exception, e:
+        print traceback.format_exc()
+        return '[]'
+
+
+def safe_str_to_dict(value):
+    try:
+        if isinstance(value, dict):
+            return value
+        if value:
+            return eval(value)
+        else:
+            return None
+    except Exception, e:
+        print traceback.format_exc()
+        return {}
+
+
+#把datetime转成字符串
+def datetime_toString(dt):
+    return dt.strftime(time_formate_str)
+
+#把字符串转成datetime
+def string_toDatetime(string):
+    return datetime.datetime.strptime(string, time_formate_str)
+
+#把字符串转成时间戳形式
+def string_toTimestamp(strTime):
+    return time.mktime(string_toDatetime(strTime).timetuple())
+
+#把时间戳转成字符串形式
+def timestamp_toString(stamp):
+    return time.strftime(time_formate_str, time.localtime(stamp))
+
+#把datetime类型转成时间戳形式
+def datetime_toTimestamp(dateTim):
+    return time.mktime(dateTim.timetuple())
+
+
+def is_datetime_equal(a_date_time, b_date_time):
+    if a_date_time.year == b_date_time.year and a_date_time.month == b_date_time.month \
+            and a_date_time.day == b_date_time.day:
+        return True
+    return False
+
+
+def time_delta_toString(timeDelta):
+    return '{:02}:{:02}:{:02}'.format(timeDelta.seconds // 3600, timeDelta.seconds % 3600 // 60, timeDelta.seconds % 60)
