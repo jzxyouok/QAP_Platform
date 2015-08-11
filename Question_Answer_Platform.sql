9f4a2a722439bdecb90c22abfa75679bcd86a7e1
cls1991 <1553556149@qq.com>
2015-08-11 23:39:37 +0800

    更新数据表

--- Question_Answer_Platform.sql
+++ Question_Answer_Platform.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.25, for osx10.10 (x86_64)
+-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
 --
--- Host: 192.168.1.106    Database: Question_Answer_Platform
+-- Host: localhost    Database: Question_Answer_Platform
 -- ------------------------------------------------------
--- Server version	5.5.44-0ubuntu0.14.10.1
+-- Server version	5.1.73
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,14 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `Question_Answer_Platform`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `Question_Answer_Platform` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `Question_Answer_Platform`;
 
 --
 -- Table structure for table `tb_account`
@@ -523,4 +531,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-08-08 21:25:02
+-- Dump completed on 2015-08-11 22:22:57
